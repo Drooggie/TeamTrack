916f2dcaeb114937babcf20dd77426df6a5b7bc1
Drooggie <oraznorlan2@gmail.com>
2024-12-24 21:37:45 +0500

    index method of emloyee added, new routes added

--- src/app/Http/Controllers/EmployeeController.php
+++ src/app/Http/Controllers/EmployeeController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Actions\Department\UpsertEmployeeAction;
 use App\DataTransferObjects\EmployeeData;
+use App\Http\Requests\GetEmployeeRequest;
 use App\Http\Requests\UpsertEmployeeRequest;
 use App\Http\Resources\EmployeeResource;
 use App\Models\Employee;
 use Illuminate\Http\Response;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class EmployeeController extends Controller
 {
     public function __construct(
         private readonly UpsertEmployeeAction $upsertEmployee
     ) {}
+
+    public function index()
+    {
+        $employees = QueryBuilder::for(Employee::class)
+            ->allowedFilters(['full_name', 'job_title', 'email', 'department.name'])
+            ->allowedIncludes(['department'])
+            ->paginate(10);
+
+        return EmployeeResource::collection($employees);
+    }
 
     public function store(UpsertEmployeeRequest $request)
     {
@@ -22,6 +34,15 @@ class EmployeeController extends Controller
         return EmployeeResource::make($employee)
             ->response()
             ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function update(UpsertEmployeeRequest $request, $employeeUuid): Response
+    {
+        $employee = Employee::where('uuid', $employeeUuid)->first();
+
+        $this->upsert($request, $employee);
+
+        return response()->noContent();
     }
 
     public function upsert(UpsertEmployeeRequest $request, Employee $employee): Employee
--- src/routes/api/v1.php
+++ src/routes/api/v1.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\EmployeeController;
-use App\Models\Employee;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/departments', [DepartmentController::class, 'index'])->name('v1.departments.index');
@@ -11,5 +9,10 @@ Route::get('/departments/{departmentUuid}', [DepartmentController::class, 'show'
 Route::post('/departments', [DepartmentController::class, 'store'])->name('v1.departments.store');
 Route::patch('/departments/{departmentUuid}', [DepartmentController::class, 'update'])->name('v1.departments.update');
 
-Route::get('/employees/{employeeUuid}', [EmployeeController::class, 'show'])->name('v1.employees.show');
-Route::post('/employees', [EmployeeController::class, 'store'])->name('v1.employees.store');
+
+Route::get('/employees', [EmployeeController::class, 'index'])->name('v1.employees.index');
+
+Route::group(['prefix' => '/employees'], function () {
+    Route::get('/{employeeUuid}', [EmployeeController::class, 'show'])->name('v1.employees.show');
+    Route::post('/', [EmployeeController::class, 'store'])->name('v1.employees.store');
+});
